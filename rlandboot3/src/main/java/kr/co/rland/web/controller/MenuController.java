ebac76352d17a0f1af9e2a0147839cfea1200728
song <dhyn.song@gmail.com>
2023-03-16 17:06:42 +0900

    jsp tiles 미지원으로 timeleaf로 변경 전

--- rlandboot3/src/main/java/kr/co/rland/web/controller/MenuController.java
+++ rlandboot3/src/main/java/kr/co/rland/web/controller/MenuController.java
@@ -1,15 +1,17 @@
 package kr.co.rland.web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+@Controller
 @RequestMapping("/menu/")
 public class MenuController {
 
 	@RequestMapping("list")
 	public String menuList() {
-		return "menuList";
+		return "menulist";
 	}
 	
 	@RequestMapping("detail")
