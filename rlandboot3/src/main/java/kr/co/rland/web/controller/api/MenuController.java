ee25c4bd99e1255e2352c5a8e0d2badf68face09
song <dhyn.song@gmail.com>
2023-03-31 18:10:00 +0900

    vue 연습

--- rlandboot3/src/main/java/kr/co/rland/web/controller/api/MenuController.java
+++ rlandboot3/src/main/java/kr/co/rland/web/controller/api/MenuController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,8 +59,10 @@ public class MenuController {
 	}
 	
 	@PostMapping
-	public String insert(@PathVariable("id") int id){
+	public String insert(@RequestBody Menu menu){
+		System.out.println(menu.getName());
+		System.out.println(menu.getPrice());
 		
-		return "menu insert " + id;
+		return "menu insert " + menu.getName() + menu.getPrice();
 	}
 }
--- rlandboot3/src/main/resources/static/js/admin/menu/list2.js
+++ rlandboot3/src/main/resources/static/js/admin/menu/list2.js
@@ -1,51 +1,114 @@
 Vue
-.createApp({
-	data(){
-		return{
-			test:"hell"
-		};
-	},
-	methods:{
-		/*async*/ categoryClickHandler(e){
-			console.log("click");
-			this.load();
-//			let response = await fetch("/menus");
-//			let list = await response.json();
-//			console.log(list);
-			
-			
-//			this.load(e.target.dataset.id);
-//			await this.load2(e.target.dataset.id);
-//			this.load(e.target.dataset.id);
-//			console.log("click-end");
+	.createApp({
+		data() {
+			return {
+				test: "hell",
+				list: [
+					{ id: 1, name: "아메리카노오", price: 5000 },
+					{ id: 2, name: "카페라떼", price: 5500 }
+				],
+				isShowRegForm: true
+				,
+				menu: { name: "", price: "" }
+			};
+		},
+		methods: {
 
+			menuSaveHandler(e) {
+				var myHeaders = new Headers();
+				myHeaders.append("Content-Type", "application/json");
+
+				var raw = JSON.stringify({
+					"name": this.menu.name,
+					"price": this.menu.price
+				});
+
+				var requestOptions = {
+					method: 'POST',
+					headers: myHeaders,
+					body: raw,
+					redirect: 'follow'
+				};
+
+				fetch("http://localhost:8080/menus", requestOptions)
+					.then(response => response.text())
+					.then(result => console.log(result))
+					.catch(error => console.log('error', error));
+					
+				console.log(this.menu.name);
+			},
+
+		/*async*/ categoryClickHandler(e) {
+				console.log("click");
+				this.load();
+				//			let response = await fetch("/menus");
+				//			let list = await response.json();
+				//			console.log(list);
+
+
+				//			this.load(e.target.dataset.id);
+				//			await this.load2(e.target.dataset.id);
+				//			this.load(e.target.dataset.id);
+				//			console.log("click-end");
+
+			},
+
+			menuAddHandler(e) {
+				this.isShowRegForm = !this.isShowRegForm;
+				console.log("메뉴추가");
+
+			},
+
+			load() {
+				//			let response = await fetch("/menus");
+				//			let list =   response.json();
+				//			console.log(list);
+
+				fetch("/menus")
+					.then(response => {
+						return response.json();
+					})
+					.then(list => {
+						this.list = list;
+					})
+					.catch(error => {
+						console.log("에러");
+					});
+
+				//			return new Promise(resolve=>{
+				//				setTimeout(()=>{
+				//			console.log("load");
+				//			},2000)});
+			},
+
+			//		load(cid){
+			//			return new Promise(resolve=>{
+			//				setTimeout(()=>{
+			//			console.log("load");
+			//			},2000)});
+			//		},
+			//		load2(cid){
+			//			return new Promise(resolve=>{
+			//				setTimeout(()=>{
+			//			console.log("load2");
+			//			},2000)
+			//			});
+			//		},
 		},
 
-		async load(cid){
-			let response = await fetch("/menus");
-			let list =   response.json();
-			console.log(list);
+		beforeCreate() { console.log("beforeCreate") },
+		created() { console.log("created") },
+		beforeMount() { console.log("beforeMount") },
+		mounted() {
+			this.load(),
+				console.log("mounted")
+		},
 
-//			return new Promise(resolve=>{
-//				setTimeout(()=>{
-//			console.log("load");
-//			},2000)});
-//		},
+		beforeUpdate() { console.log("beforeUpdate") },
+		updated() { console.log("updated") },
+		beforeUnmount() { console.log("beforeUnmount") },
+		unmounted() { console.log("unmounted") }
 
-//		load(cid){
-//			return new Promise(resolve=>{
-//				setTimeout(()=>{
-//			console.log("load");
-//			},2000)});
-//		},
-//		load2(cid){
-//			return new Promise(resolve=>{
-//				setTimeout(()=>{
-//			console.log("load2");
-//			},2000)
-//			});
-//		}
-	},
-	
-}})
-.mount("#main-section");
+
+	})
+	.mount("#main-section");
